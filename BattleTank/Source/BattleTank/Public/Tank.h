1d47d3e6f2736e8efd79b10b75853c4274c36c47
Jan Valušek <janvalusek123@gmail.com>
2017-09-28 20:21:16 +0200

    BT40 CHALLENGE - Turret Rotation Pt.2

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -31,7 +31,7 @@ private:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 	UPROPERTY(EditAnywhere, Category = Firing)
-	float LaunchSpeed = 100000; // TODO find sensible default
+	float LaunchSpeed = 4000;
 
 public:
 	void AimAt(FVector HitLocation);
